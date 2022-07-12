17a145336f8458c793641f61d137e6507507643b
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2022-07-12 13:34:49 +0300

    Username and password moved to request body

--- src/main/java/exceptions/InvalidLoginParametersException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InvalidLoginParametersException extends RuntimeException {
-
-	public InvalidLoginParametersException() {
-        super("Login or password parameters cannot be blank or null");
-    }
-	
-}
--- src/main/java/server/http/LoginHandler.java
+++ src/main/java/server/http/LoginHandler.java
@@ -1,14 +1,14 @@
 package server.http;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.Optional;
+
+import org.json.JSONObject;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import exceptions.InvalidLoginParametersException;
 import model.User;
 import repository.user.UserRepository;
 import repository.user.UserRepositoryImpl;
@@ -28,7 +28,6 @@ public class LoginHandler implements HttpHandler {
 	public void handle(HttpExchange exchange) throws IOException {
 		try {
 			if(exchange.getRequestMethod().equals("POST")) {
-				System.out.println("Starting login handle...");
 				this.handlePost(exchange);
 			} else {
 				System.out.println(exchange.getRequestMethod());
@@ -43,15 +42,10 @@ public class LoginHandler implements HttpHandler {
 	}
 	
 	private void handlePost(HttpExchange exchange) throws IOException {
-		String uri = exchange.getRequestURI().toString();
-		Map<String, String> params = Utils.getQueryParams(uri);
-		String username = params.get("username");
-		String password = params.get("password");
-		if(username == null || password == null 
-				|| username.isBlank() || password.isBlank()) 
-			throw new InvalidLoginParametersException();
+		JSONObject object = Utils.getRequestBody(exchange);
+		String username = object.getString("username");
+		String password = object.getString("password");
 		Optional<User> opt = this.userRepo.getByUsername(username);
-		System.out.println(opt.toString());
 		if(opt.isPresent() && opt.get().getPassword().equals(password)) {
 			Headers responseHeaders = exchange.getResponseHeaders();
 	        responseHeaders.add("Access-Control-Allow-Origin", "*");
